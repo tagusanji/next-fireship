f8d83039fd840e3f15d6a46e99df58024259aba2
tag-za <98021828+tag-za@users.noreply.github.com>
2023-08-10 17:49:52 +0200

    added hullo

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,11 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <div>
+        <p>hullo</p>
+      </div>
+    </main>
+  );
 }
